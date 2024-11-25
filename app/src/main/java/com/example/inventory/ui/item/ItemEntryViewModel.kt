4a1c0ab7d7d201419e34af8e1a9e9584f5b5fe00
Fabio Cizinande <fabiocizinande80@gmail.com>
2024-11-25 16:35:44 -0300

    [+] Atualizacao viewmodel

--- app/src/main/java/com/example/inventory/ui/item/ItemEntryViewModel.kt
+++ app/src/main/java/com/example/inventory/ui/item/ItemEntryViewModel.kt
@@ -1,55 +1,4 @@
-/*
- * Copyright (C) 2023 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
-package com.example.inventory.ui.item
-
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.setValue
-import androidx.lifecycle.ViewModel
-import com.example.inventory.data.Item
-import java.text.NumberFormat
-
-/**
- * ViewModel to validate and insert items in the Room database.
- */
-class ItemEntryViewModel : ViewModel() {
-
-    /**
-     * Holds current item ui state
-     */
-    var itemUiState by mutableStateOf(ItemUiState())
-        private set
-
-    /**
-     * Updates the [itemUiState] with the value provided in the argument. This method also triggers
-     * a validation for input values.
-     */
-    fun updateUiState(itemDetails: ItemDetails) {
-        itemUiState =
-            ItemUiState(itemDetails = itemDetails, isEntryValid = validateInput(itemDetails))
-    }
-
-    private fun validateInput(uiState: ItemDetails = itemUiState.itemDetails): Boolean {
-        return with(uiState) {
-            name.isNotBlank() && price.isNotBlank() && quantity.isNotBlank()
-        }
-    }
-}
-
+// No need to copy, this is part of starter code
 /**
  * Represents Ui State for an Item.
  */
@@ -70,6 +19,13 @@ data class ItemDetails(
  * not a valid [Double], then the price will be set to 0.0. Similarly if the value of
  * [ItemDetails.quantity] is not a valid [Int], then the quantity will be set to 0
  */
+
+private fun validateInput(uiState: ItemDetails = itemUiState.itemDetails): Boolean {
+    return with(uiState) {
+        name.isNotBlank() && price.isNotBlank() && quantity.isNotBlank()
+    }
+}
+
 fun ItemDetails.toItem(): Item = Item(
     id = id,
     name = name,
