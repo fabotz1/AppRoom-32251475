980d99fd6d4442260de3705c9d631befee1abdbe
Fabio Cizinande <fabiocizinande80@gmail.com>
2024-11-25 16:50:52 -0300

    [+] Alteracoes de funcionamento

--- app/src/main/java/com/example/inventory/data/Item.kt
+++ app/src/main/java/com/example/inventory/data/Item.kt
@@ -16,16 +16,15 @@
 
 package com.example.inventory.data
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 
 /**
  * Entity data class represents a single row in the database.
  */
-import androidx.room.Entity
-
 @Entity(tableName = "items")
-
 data class Item(
-    @PrimaryKey
+    @PrimaryKey(autoGenerate = true)
     val id: Int = 0,
     val name: String,
     val price: Double,
